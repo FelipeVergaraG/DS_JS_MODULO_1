d871201362f8eed1c6efceecc775ef3b886117e3
FelipeVergaraG <felipe.vergara.ingeniero@gmail.com>
2026-09-19 23:14:06 -0500

    Clase 2: variables, tipos, operadores y TypeScript

--- Clase_2/demo.ts
+++ Clase_2/demo.ts
@@ -7,3 +7,8 @@ const precio: number = 5;
 const envio: number = 10;
 console.log(precio + envio);
 
+//REGLA
+//Tú nunca tocas el .js a mano:es automático. 
+// Editas la receta (.ts), no el plato cocinado (.js). 
+// El comensal (Node) come el plato, no lee la receta.
+
